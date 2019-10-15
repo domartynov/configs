e0ee98fadc394924b09856140b56565088bfe468
Dzmitry Martynau <domartynov@gmail.com>
2019-10-15 00:32:42 -0400

    fix setup.cmd (symlinks dont work, use hardlinks)

--- setup.cmd
+++ setup.cmd
@@ -4,19 +4,19 @@ cd %~dp0
 set h=%userprofile%
 
 del %h%\.vimrc.keymap
-mklink %h%\.vimrc.keymap .vimrc.keymap
+mklink /h %h%\.vimrc.keymap .vimrc.keymap
 
 del %h%\.vimrc
-mklink %h%\.vimrc .vimrc
+mklink /h %h%\.vimrc .vimrc
 
 del %h%\.nvimrc
-mklink %h%\.nvimrc .nvimrc
+mklink /h %h%\.nvimrc .nvimrc
 
 del %h%\.ideavimrc
-mklink %h%\.ideavimrc .ideavimrc
+mklink /h %h%\.ideavimrc .ideavimrc
 
 md %appdata%\..\Local\nvim\
 del %appdata%\..\Local\nvim\init.vim
-mklink %appdata%\..\Local\nvim\init.vim init.vim
+mklink /h %appdata%\..\Local\nvim\init.vim init.vim
 
 cd %cwd%
