5786c7dd9bd2789400cb8a34420b74dc1b3b8423
Dmitry Martynov <domartynov@gmail.com>
2019-11-10 11:58:05 -0500

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+d0=$(realpath $(dirname $0))
+
+function mklink () {
+	local target=${2:-$HOME}/$1
+	mkdir -p $(dirname $target)
+	ln -vf "$d0/$1" "${2:-$HOME}/$1"
+}
+
+mklink .vimrc.keymap
+mklink .vimrc.base
+mklink .vimrc
+mklink .nvimrc
+mklink .ideavimrc
+mklink init.vim $HOME/.config/nvim
+
